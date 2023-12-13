a6e824bafdaa52834c4f0c348e009c84891655f9
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-13 21:16:52 +0200

    ✨ feat(AddItem Component): Add content
    The content is added to the AddItem Component

--- src/components/AddItem/AddItem.jsx
+++ src/components/AddItem/AddItem.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './AddItem.css'
+import { MdCancel } from "react-icons/md";
+import { MdDeleteForever } from "react-icons/md";
 
 const AddItem = () => {
   return (
-    <div>
-      
+    <div className='addItemWrapper'>
+        <div className='item'>
+            <div className="itemContent">
+                <MdCancel id='cancel'/>
+                <p>Item 4</p>
+            </div>
+            <MdDeleteForever id='delete'/>
+        </div>
+        <hr id='hr'/>
     </div>
   )
 }
