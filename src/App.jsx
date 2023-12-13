2a83c1a5bd8f9b719ed214e3446036108c246648
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-13 18:14:20 +0200

    ✨ feat(Application): Add Content
    Add the header content for the App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { FaMoon } from "react-icons/fa";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+
+      {/* create the header element */}
+      <header className='headerWrapper'>
+
+        {/* insert the content */}
+        <div className='header-content'>
+          <div className='todo'>
+            <h1>TODO</h1>
+          </div>
+          <div className='moon'>
+            <FaMoon />
+          </div>
+        </div>
+      </header>
+
+    </div>
   )
 }
 
