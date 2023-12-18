aec9b97dc024bdfea5ed41c14371d27c77664262
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-18 10:00:27 +0200

    🦄 refactor(AddIItem Component): Add Function
    Add the toggle status function and the functionality to render different icons conditionally

--- src/components/AddItem/AddItem.jsx
+++ src/components/AddItem/AddItem.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import './AddItem.css'
 import { MdCancel } from "react-icons/md";
 import { MdDeleteForever } from "react-icons/md";
+import { MdCheck } from "react-icons/md";
 
-const AddItem = ( {data, deleteHandler} ) => {
+
+const AddItem = ( {data, deleteHandler, updateStatus} ) => {
   return (
     <div>
         <div className='item'>
             <div className="itemContent">
-                <MdCancel className='cancel'/>
-                <p>{data.todoItem}</p>
+                {data.completed ? <MdCheck className='check' onClick={() => updateStatus(data.id)}/> :
+                <MdCancel className='cancel' onClick={() => updateStatus(data.id)}/>}
+                {/* <MdCancel className='cancel' onClick={() => updateStatus(data.id)}/> */}
+                <p style={data.completed ? {textDecoration: 'line-through'} : null}>{data.todoItem}</p>
             </div>
             <MdDeleteForever className='delete' onClick={() => deleteHandler(data.id)}/>
         </div>
