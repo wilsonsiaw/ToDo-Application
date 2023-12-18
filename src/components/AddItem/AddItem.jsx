cb4fe24703365b0376fab0866ebbec924de35897
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-18 07:50:12 +0200

    ✨ feat(AddItems Component): Add prop
    Add the title prop to the AddItem Component

--- src/components/AddItem/AddItem.jsx
+++ src/components/AddItem/AddItem.jsx
@@ -3,13 +3,13 @@ import './AddItem.css'
 import { MdCancel } from "react-icons/md";
 import { MdDeleteForever } from "react-icons/md";
 
-const AddItem = () => {
+const AddItem = ( {title} ) => {
   return (
     <div>
         <div className='item'>
             <div className="itemContent">
                 <MdCancel id='cancel'/>
-                <p>Item 4</p>
+                <p>{title}</p>
             </div>
             <MdDeleteForever id='delete'/>
         </div>
